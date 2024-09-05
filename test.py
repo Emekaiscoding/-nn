d5915219ae98da7e0e21eff0061e681a694fb848
Emekaiscoding <chukwuemekaanyanwu322@gmail.com>
2024-09-05 09:37:40 +0100

    Adding subtraction comments

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ def add_numbers(a, b):
     """
     return a + b
 
+#Subtraction function
 def subtract_numbers(a, b):
     """
     This function takes two numbers as input and returns their difference.
